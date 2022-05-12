74fff11e637bfbe8b05a6a39009b46142d6d1d4b
Adriel Bustos <adriel.bustos01@gmail.com>
2022-05-11 22:26:23 -0300

    add map

--- src/Company.ts
+++ src/Company.ts
@@ -1,12 +1,14 @@
 import faker from "@faker-js/faker";
+import { Mappable } from "./CustomMap";
 
-export class Company {
+export class Company implements Mappable {
     name: string;
     catchPhrase: string;
     location: {
         lat: number;
         lng: number;
-    }
+    };
+    color: string;
     constructor() {
         this.name = faker.company.companyName();
         this.catchPhrase = faker.company.catchPhrase()
@@ -14,5 +16,9 @@ export class Company {
             lat: parseFloat(faker.address.latitude()),
             lng: parseFloat(faker.address.longitude())
         };
+        this.color = "red";
+    }
+    markerContent():string {
+        return `company Name: ${this.name}`
     }
 }
--- src/User.ts
+++ src/User.ts
@@ -1,16 +1,22 @@
 import faker from '@faker-js/faker';
+import { Mappable } from "./CustomMap";
 
-export class User {
+export class User implements Mappable {
     name: string;
     location: {
         lat: number;
         lng: number;
     };
+    color: string;
     constructor() {
         this.name = faker.name.firstName();
         this.location = {
             lat: parseFloat(faker.address.latitude()),
             lng: parseFloat(faker.address.longitude())
         };
+        this.color = "blue";
+    }
+    markerContent():string {
+        return `user Name: ${this.name}`
     }
 }
